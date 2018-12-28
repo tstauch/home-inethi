afd4761fa6f06f00f3a9286a5c7aa2fb1652a00a
Torsten Stauch <torsten@appshed.com>
2018-12-28 10:32:44 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-3']
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+          xxxapiUrl: 'http://localhost:4000',
+          apiUrl: 'https://api.inethi.net/api/v2',
+          dreamFactoryAPIKey: '7a2ccec1e61a29042198af984d6b8a45bee98d45342819819a9c3b97aef3c16c'
+        })
+    }
+}
